bfdf26321180dfba54b1471e30fd4988c58bae68
Diogo Guimaraes <mail@diogoguimaraes.pt>
2023-01-17 14:58:01 +0000

    unfinished even with google help... but not blankgit add .

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,60 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
+
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+// "Google" help - don't fully understand
+// started with const intervalId = setInterval
+
+//more precisaly
+//  this.currentTime++;
+//  const intervalId = setInterval(printTimeCallback, 1000);
+
+//this.intervalId = setInterval( () => {} )
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) printTimeCallback();
+    }, 1000);
   }
 
-  getMinutes() {
-    // ... your code goes here
+
+//const intervalId = setInterval(callbackFunction, delay);
+// still confused with Math.Something being used with all  kind of type of data/values
+
+  getMinutes() {    
+    if (this.currentTime === 0) return 0; // Justmean takes this with or without this verification/condition
+    return Math.floor(this.currentTime / 60); // goggled (adapted) code
   }
 
   getSeconds() {
-    // ... your code goes here
-  }
+    if (this.currentTime === 0) return 0; // Justmean takes this with or without this verification/condition
+    return Math.floor(this.currentTime % 60); // adapted from above googled code
+  } 
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+// brain freeze 
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime === 0;
   }
 
   split() {
-    // ... your code goes here
-  }
+// brain freeze  
 }
+}
+
+
+
+  
+
+
